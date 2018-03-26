c21f0f90990936444bc4ec91801ac3101cd9f0c4
Leonardo Silva <leonardosilva@macbook.local>
2018-03-26 11:10:42 -0300

    Adding report scripts

---
+++ generate_performance_report.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat > report.html << EOF
+  <!DOCTYPE html>
+  <html>
+  <head>
+      <title>Performance Report</title>
+
+  </head>
+
+  <body>
+    <h1>Performance Report</h1>
+  </body>
+  </html>
+EOF
---
+++ generate_security_report.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat > report.html << EOF
+  <!DOCTYPE html>
+  <html>
+  <head>
+      <title>Security Report</title>
+
+  </head>
+
+  <body>
+    <h1>Security Report</h1>
+  </body>
+  </html>
+EOF
